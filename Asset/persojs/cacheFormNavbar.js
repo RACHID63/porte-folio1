a020ab0442a677d85b2395ae5a3f710e00b788f4
RACHID LAIEB <rachid.laieb@gmail.com>
2023-10-25 15:12:01 +0200

    up to dat btn gallery

--- Asset/persojs/cacheFormNavbar.js
+++ Asset/persojs/cacheFormNavbar.js
@@ -1,8 +1,8 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Vérifiez si la page actuelle est l'index
-    var myForm = document.getElementById('myForm');
-    if (window.location.pathname.includes('index.html')) {
-        // Si oui, ajoutez la classe "cacheForme" à la form
-        myForm.classList.add('cacheForme');
-    }
-});
+// document.addEventListener('DOMContentLoaded', function() {
+//     // Vérifiez si la page actuelle est l'index
+//     var myForm = document.getElementById('myForm');
+//     if (window.location.pathname.includes('index.html')) {
+//         // Si oui, ajoutez la classe "cacheForme" à la form
+//         myForm.classList.add('cacheForme');
+//     }
+// });
